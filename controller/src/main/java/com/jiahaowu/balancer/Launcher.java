4c0d49e9ac89eb2573d1e56cf93893fe1f74ec63
Jiahao Wu <visylar@gmail.com>
2017-04-29 00:41:11 -0400

    code update

--- controller/src/main/java/com/jiahaowu/balancer/Launcher.java
+++ controller/src/main/java/com/jiahaowu/balancer/Launcher.java
@@ -2,12 +2,13 @@ package com.jiahaowu.balancer;
 
 /**
  * Created by jiahao on 4/28/17.
- *
+ * <p>
  * Entry point for load balance manager
  */
 public class Launcher {
     private static final String[] STATS = {"SERVER", "CLIENT", "BACKUP"};
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        System.out.println("Hello");
     }
 }

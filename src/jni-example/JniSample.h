ab2cce82d45d4ec95c8cb41d6b1b3e77ec0af70c
Kyunghun Lee <leekh3@umd.edu>
2017-05-03 16:26:52 -0400

    doc updated

--- src/jni-example/JniSample.h
+++ src/jni-example/JniSample.h
@@ -24,6 +24,25 @@ extern "C" {
  * Signature: ()I
  */
 JNIEXPORT jint JNICALL Java_JniSample_Monte (JNIEnv *env, jobject obj, jint num_rand, jint num_repeat);
+JNIEXPORT jdouble JNICALL Java_JniSample_Bench (JNIEnv *env, jobject obj, jint num);
+
+void test01 ( int n );
+double test02 ( int n );
+void test03 ( int n );
+
+int isamax ( int n, float x[], int incx );
+void matgen ( int lda, int n, float a[], float x[], float b[] );
+void msaxpy ( int nr, int nc, float a[], int n, float x[], float y[] );
+void msaxpy2 ( int nr, int nc, float a[], int n, float x[], float y[] );
+int msgefa ( float a[], int lda, int n, int ipvt[] );
+int msgefa2 ( float a[], int lda, int n, int ipvt[] );
+void saxpy ( int n, float a, float x[], int incx, float y[], int incy );
+float sdot ( int n, float x[], int incx, float y[], int incy );
+int sgefa ( float a[], int lda, int n, int ipvt[] );
+void sgesl ( float a[], int lda, int n, int ipvt[], float b[], int job );
+void sscal ( int n, float a, float x[], int incx );
+void sswap ( int n, float x[], int incx, float y[], int incy );
+void timestamp ( void );
 
 #ifdef __cplusplus
 }
--- src/jni-example/JniSample.java
+++ src/jni-example/JniSample.java
@@ -4,6 +4,7 @@ public class JniSample {
       System.loadLibrary("JniSample");
    }
 	public native int Monte(int num_rand, int num_repeat);
+  public native double Bench(int n);
     
     public static void main(String[] args) {
 
@@ -11,9 +12,11 @@ public class JniSample {
         int num_repeat = 3;
 
         int count_total = new JniSample().Monte(num_rand,num_repeat);
+        double bench_result = new JniSample().Bench(1000);
 
         double pi_result = count_total /(double) num_rand / num_repeat * 4 ;
 
+        System.out.println("Bench_result:"+bench_result);
         System.out.println("Total Random Number:"+num_rand);
         System.out.println("Repeated:"+num_repeat);
         System.out.println("Calculated PI:"+pi_result);

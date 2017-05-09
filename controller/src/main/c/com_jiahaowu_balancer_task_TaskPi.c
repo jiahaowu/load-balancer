7f059984fe24abdab4b0ec94f1d2d5f6dbc760a9
Kyunghun Lee <leekh3@umd.edu>
2017-05-09 16:37:53 -0400

    updated

--- controller/src/main/c/com_jiahaowu_balancer_task_TaskPi.c
+++ controller/src/main/c/com_jiahaowu_balancer_task_TaskPi.c
@@ -1,73 +1,71 @@
 #include "com_jiahaowu_balancer_task_TaskPi.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
-#include <omp.h>
-#include <limits.h>
-
-#include <unistd.h>
-#include <time.h>
-#include <sys/time.h>
+/* The array for the state vector */
+static unsigned long long mt[NN]; 
+/* mti==NN+1 means mt[NN] is not initialized */
+static int mti=NN+1; 
 
 JNIEXPORT jint JNICALL Java_com_jiahaowu_balancer_task_TaskPi_Monte(
     JNIEnv *env, jobject obj, jint num_rand, jint num_repeat) {
      int my_cpu_id,numthreads;
 
-     #ifdef _OPENMP
-     numthreads=omp_get_max_threads();
-     #else
-     numthreads=1;
-     #endif
+    #ifdef _OPENMP
+    numthreads=omp_get_max_threads();
+    #else
+    numthreads=1;
+    #endif
+    
+    int i,j;
+    int randInts_A[1], randInts_B[1];
+    int count[numthreads];
+    int count_total = 0;
+    double radius;
+    double rand_double_A, rand_double_B;
+    double pi_result;
+    int truerand;
 
-     int i,j;
-     int randInts_A[1], randInts_B[1];
-     int count[numthreads];
-     int count_total = 0;
-     double radius;
-     double rand_double_A, rand_double_B;
-     double pi_result;
-     int truerand;
+    /* Measure runtime */
+    double totaltime = 0;
+    struct timeval  tv1, tv2;
+    gettimeofday(&tv1, NULL);
 
-     /* Measure runtime */
-     double totaltime = 0;
-     struct timeval  tv1, tv2;
-     gettimeofday(&tv1, NULL);
+    for (i=0; i<numthreads; i++) {
+        count[i] = 0;
+    }
 
-     for (i=0; i<numthreads; i++) {
-         count[i] = 0;
-     }
+    for (j=0; j<num_repeat; j++) {
+      #pragma omp parallel private(i, my_cpu_id, randInts_A, randInts_B, radius, rand_double_A, rand_double_B)
+      {
+        #ifdef _OPENMP
+        my_cpu_id=omp_get_thread_num();
+        
+        //generate random seed
+        if (j%1000 == 0) {
+            truerand = rdtsc();
+            init_genrand64(truerand * (my_cpu_id+5));
+        }
+        #else
+        my_cpu_id=0;
+        #endif
+        
+        #pragma omp for
+        for(i=1; i<num_rand; i++) {
+        
+        // randInts_A[0] = (double) genrand();
+        // randInts_B[0] = (double) random();
+        rand_double_A = genrand64_real2();
+        rand_double_B = genrand64_real2();
 
-     for (j=0; j<num_repeat; j++) {
-       #pragma omp parallel private(i, my_cpu_id, randInts_A, randInts_B, radius, rand_double_A, rand_double_B)
-       {
-         #ifdef _OPENMP
-         my_cpu_id=omp_get_thread_num();
-         truerand = rdtsc();
-         srandom(truerand * (my_cpu_id+5));
-         #else
-         my_cpu_id=0;
-         #endif
-
-         #pragma omp for
-         for(i=1; i<num_rand; i++) {
-
-         randInts_A[0] = (double) random();
-         randInts_B[0] = (double) random();
-         rand_double_A = randInts_A[0] / (double)INT_MAX;
-         rand_double_B = randInts_B[0] / (double)INT_MAX;
-
-         radius = rand_double_A * rand_double_A + rand_double_B *rand_double_B;
-         if (radius <= 1) count[my_cpu_id]++;
-
-         }
-       }
-     }
-
-     for (i=0;i<numthreads;i++) {
-         count_total += count[i];
-     }
+        radius = rand_double_A * rand_double_A + rand_double_B *rand_double_B;
+        if (radius <= 1) count[my_cpu_id]++;
+        //printf("Random Integer: %d, Random double: %lf\n", randInt, rand_double);
+        }
+      }
+    }
+    
+    for (i=0;i<numthreads;i++) {      
+        count_total += count[i];
+    }
 
    return count_total;
 
@@ -77,4 +75,101 @@ unsigned long long rdtsc() {
     unsigned int lo, hi;
     __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
     return ((unsigned long long)hi << 32) | lo;
+}
+
+/* initializes mt[NN] with a seed */
+void init_genrand64(unsigned long long seed)
+{
+    mt[0] = seed;
+    for (mti=1; mti<NN; mti++) 
+        mt[mti] =  (6364136223846793005ULL * (mt[mti-1] ^ (mt[mti-1] >> 62)) + mti);
+}
+
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+void init_by_array64(unsigned long long init_key[],
+             unsigned long long key_length)
+{
+    unsigned long long i, j, k;
+    init_genrand64(19650218ULL);
+    i=1; j=0;
+    k = (NN>key_length ? NN : key_length);
+    for (; k; k--) {
+        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 62)) * 3935559000370003845ULL))
+          + init_key[j] + j; /* non linear */
+        i++; j++;
+        if (i>=NN) { mt[0] = mt[NN-1]; i=1; }
+        if (j>=key_length) j=0;
+    }
+    for (k=NN-1; k; k--) {
+        mt[i] = (mt[i] ^ ((mt[i-1] ^ (mt[i-1] >> 62)) * 2862933555777941757ULL))
+          - i; /* non linear */
+        i++;
+        if (i>=NN) { mt[0] = mt[NN-1]; i=1; }
+    }
+
+    mt[0] = 1ULL << 63; /* MSB is 1; assuring non-zero initial array */ 
+}
+
+/* generates a random number on [0, 2^64-1]-interval */
+unsigned long long genrand64_int64(void)
+{
+    int i;
+    unsigned long long x;
+    static unsigned long long mag01[2]={0ULL, MATRIX_A};
+
+    if (mti >= NN) { /* generate NN words at one time */
+
+        /* if init_genrand64() has not been called, */
+        /* a default initial seed is used     */
+        if (mti == NN+1) 
+            init_genrand64(5489ULL); 
+
+        for (i=0;i<NN-MM;i++) {
+            x = (mt[i]&UM)|(mt[i+1]&LM);
+            mt[i] = mt[i+MM] ^ (x>>1) ^ mag01[(int)(x&1ULL)];
+        }
+        for (;i<NN-1;i++) {
+            x = (mt[i]&UM)|(mt[i+1]&LM);
+            mt[i] = mt[i+(MM-NN)] ^ (x>>1) ^ mag01[(int)(x&1ULL)];
+        }
+        x = (mt[NN-1]&UM)|(mt[0]&LM);
+        mt[NN-1] = mt[MM-1] ^ (x>>1) ^ mag01[(int)(x&1ULL)];
+
+        mti = 0;
+    }
+  
+    x = mt[mti++];
+
+    x ^= (x >> 29) & 0x5555555555555555ULL;
+    x ^= (x << 17) & 0x71D67FFFEDA60000ULL;
+    x ^= (x << 37) & 0xFFF7EEE000000000ULL;
+    x ^= (x >> 43);
+
+    return x;
+}
+
+/* generates a random number on [0, 2^63-1]-interval */
+long long genrand64_int63(void)
+{
+    return (long long)(genrand64_int64() >> 1);
+}
+
+/* generates a random number on [0,1]-real-interval */
+double genrand64_real1(void)
+{
+    return (genrand64_int64() >> 11) * (1.0/9007199254740991.0);
+}
+
+/* generates a random number on [0,1)-real-interval */
+double genrand64_real2(void)
+{
+    return (genrand64_int64() >> 11) * (1.0/9007199254740992.0);
+}
+
+/* generates a random number on (0,1)-real-interval */
+double genrand64_real3(void)
+{
+    return ((genrand64_int64() >> 12) + 0.5) * (1.0/4503599627370496.0);
 }

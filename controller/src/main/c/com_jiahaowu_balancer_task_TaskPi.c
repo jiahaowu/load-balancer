e7c551bdd4568a807bdb14a2ef254e80f8f17c9a
Jiahao Wu <visylar@gmail.com>
2017-05-09 15:48:01 -0400

    src update

--- controller/src/main/c/com_jiahaowu_balancer_task_TaskPi.c
+++ controller/src/main/c/com_jiahaowu_balancer_task_TaskPi.c
@@ -13,63 +13,63 @@
 
 JNIEXPORT jint JNICALL Java_com_jiahaowu_balancer_task_TaskPi_Monte(
     JNIEnv *env, jobject obj, jint num_rand, jint num_repeat) {
-    int my_cpu_id,numthreads;
+     int my_cpu_id,numthreads;
 
-    #ifdef _OPENMP
-    numthreads=omp_get_max_threads();
-    #else
-    numthreads=1;
-    #endif
+     #ifdef _OPENMP
+     numthreads=omp_get_max_threads();
+     #else
+     numthreads=1;
+     #endif
 
-    int i,j;
-    int randInts_A[1], randInts_B[1];
-    int count[numthreads];
-    int count_total = 0;
-    double radius;
-    double rand_double_A, rand_double_B;
-    double pi_result;
-    int truerand;
+     int i,j;
+     int randInts_A[1], randInts_B[1];
+     int count[numthreads];
+     int count_total = 0;
+     double radius;
+     double rand_double_A, rand_double_B;
+     double pi_result;
+     int truerand;
 
-    /* Measure runtime */
-    double totaltime = 0;
-    struct timeval  tv1, tv2;
-    gettimeofday(&tv1, NULL);
+     /* Measure runtime */
+     double totaltime = 0;
+     struct timeval  tv1, tv2;
+     gettimeofday(&tv1, NULL);
 
-    for (i=0; i<numthreads; i++) {
-        count[i] = 0;
-    }
+     for (i=0; i<numthreads; i++) {
+         count[i] = 0;
+     }
 
-    for (j=0; j<num_repeat; j++) {
-      #pragma omp parallel private(i, my_cpu_id, randInts_A, randInts_B, radius, rand_double_A, rand_double_B)
-      {
-        #ifdef _OPENMP
-        my_cpu_id=omp_get_thread_num();
-        truerand = rdtsc();
-        if (j==0) srandom(truerand * (my_cpu_id+5));
-        #else
-        my_cpu_id=0;
-        #endif
+     for (j=0; j<num_repeat; j++) {
+       #pragma omp parallel private(i, my_cpu_id, randInts_A, randInts_B, radius, rand_double_A, rand_double_B)
+       {
+         #ifdef _OPENMP
+         my_cpu_id=omp_get_thread_num();
+         truerand = rdtsc();
+         srandom(truerand * (my_cpu_id+5));
+         #else
+         my_cpu_id=0;
+         #endif
 
-        #pragma omp for
-        for(i=1; i<num_rand; i++) {
+         #pragma omp for
+         for(i=1; i<num_rand; i++) {
 
-        randInts_A[0] = (double) random();
-        randInts_B[0] = (double) random();
-        rand_double_A = randInts_A[0] / (double)INT_MAX;
-        rand_double_B = randInts_B[0] / (double)INT_MAX;
+         randInts_A[0] = (double) random();
+         randInts_B[0] = (double) random();
+         rand_double_A = randInts_A[0] / (double)INT_MAX;
+         rand_double_B = randInts_B[0] / (double)INT_MAX;
 
-        radius = rand_double_A * rand_double_A + rand_double_B *rand_double_B;
-        if (radius <= 1) count[my_cpu_id]++;
+         radius = rand_double_A * rand_double_A + rand_double_B *rand_double_B;
+         if (radius <= 1) count[my_cpu_id]++;
 
-        }
-      }
-    }
+         }
+       }
+     }
 
-    for (i=0;i<numthreads;i++) {
-        count_total += count[i];
-    }
+     for (i=0;i<numthreads;i++) {
+         count_total += count[i];
+     }
 
-  return count_total;
+   return count_total;
 
 }
 
